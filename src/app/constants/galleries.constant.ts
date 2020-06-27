24f22ab9918970d34374e9b8b3612e2c495d463c
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-27 21:59:42 +0200

    create mock values

---
+++ src/app/constants/galleries.constant.ts
@@ -0,0 +1,27 @@
+// TODO change values
+
+export const Galleries = [{
+  galleryId: '1',
+  title: 'Rzym',
+  dateCreated: '2015-12-15T00:00:00+00:00',
+  thumbUrl: './assets/img/galleries/rzym-2015/1-sm.jpg',
+  description: 'Kilka dni zwariowanego wypadu do Rzymu.',
+  tags: [],
+  photos: []
+}, {
+  galleryId: '2',
+  title: 'Maroko',
+  dateCreated: '2015-08-07T00:00:00+00:00',
+  thumbUrl: './assets/img/galleries/maroko-2015/1-sm.jpg',
+  description: 'Tydzień zwiedzania południowego Maroka z ojcem.',
+  tags: [],
+  photos: []
+}, {
+  galleryId: '3',
+  title: 'Tajlandia',
+  dateCreated: '2014-11-10T00:00:00+00:00',
+  thumbUrl: './assets/img/galleries/tajlandia-2014/1-sm.jpg',
+  description: 'Dwa tygodnie wycieczki po Tajlandii.',
+  tags: [],
+  photos: []
+}];
