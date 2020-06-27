2b19df8aeb51ba2d7337841a7305df4f977fae12
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-28 01:01:11 +0200

    esport years operation to method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {IGallery} from './IGallery';
 import {Galleries} from './constants/galleries.constant';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,28 @@ export class AppComponent {
   description: string;
   galleries: IGallery[];
   searchValue: any;
+  travelYearsSet;
+  travelYearsArray = [];
+  yearSearch: any = null;
+
+  createSortedYearsArray = () => {
+    this.travelYearsSet = new Set();
+    Galleries.map(travel => this.travelYearsSet.add(moment(travel.dateCreated).format('yyyy')));
+    this.travelYearsSet.forEach(year => {
+      this.travelYearsArray.push(parseInt(year, 10));
+    });
+    this.travelYearsArray.sort((a, b) => a - b);
+  }
+
+  setValue = (value) => {
+    this.yearSearch = value;
+  }
 
   constructor() {
     this.title = 'My travels';
     this.description = 'Website created for front-end technologies at university';
     this.galleries = Galleries;
     this.searchValue = '';
+    this.createSortedYearsArray();
   }
 }
