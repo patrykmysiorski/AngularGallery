9eb8e457ff223873896ec36c0b711c7a1df9597d
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-28 01:00:25 +0200

    create serach by year pipe

---
+++ src/app/pipes/search-by-year.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchByYearPipe } from './search-by-year.pipe';
+
+describe('SearchByYearPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchByYearPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
