154de835442ea1e0bfcaf9ae1acd7ca766b189f9
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-27 23:44:50 +0200

    add forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import {FormsModule} from '@angular/forms';
+import { SearchGalleriesPipe } from './pipes/search-galleries.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchGalleriesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
