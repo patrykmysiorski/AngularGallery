2428ec027e481ed9e5dc04b4d32f9776155f1f90
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-28 14:52:43 +0200

    download gallery from server

--- src/app/components/galleries/gallery/gallery.component.ts
+++ src/app/components/galleries/gallery/gallery.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {IGallery} from '../../../../intefaces/IGallery';
 import {Galleries} from '../../../constants/galleries.constant';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-gallery',
@@ -10,14 +11,27 @@ import {Galleries} from '../../../constants/galleries.constant';
 })
 export class GalleryComponent implements OnInit {
 
+  httpOptions = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json',
+        Authorization: '104'
+      }
+    )
+  };
+
   private galleryId: string;
   gallery: IGallery;
 
-  constructor(private route: ActivatedRoute) { }
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.galleryId = this.route.snapshot.paramMap.get('galleryId');
-    this.gallery = Galleries.find((item: IGallery) => item.galleryId === this.galleryId);
+    const url = 'http://project.usagi.pl/gallery/' + this.galleryId;
+    this.http.get(url, this.httpOptions).toPromise().then((response: IGallery) => {
+      this.gallery = response;
+    });
   }
 
 }
