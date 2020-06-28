3769aed374c2f760369befb2ab2eea82be4536cd
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-28 14:08:13 +0200

    add functions to connect with server

--- src/app/components/galleries/galleries/galleries.component.ts
+++ src/app/components/galleries/galleries/galleries.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IGallery} from '../../../../intefaces/IGallery';
 import {Galleries} from '../../../constants/galleries.constant';
 import * as moment from 'moment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-galleries',
@@ -9,6 +10,16 @@ import * as moment from 'moment';
   styleUrls: ['./galleries.component.scss']
 })
 export class GalleriesComponent implements OnInit {
+
+  httpOptions = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json',
+        Authorization: '104'
+      }
+    )
+  };
+
   title = 'AngularGallery';
   description: string;
   galleries: IGallery[];
@@ -26,12 +37,21 @@ export class GalleriesComponent implements OnInit {
     this.travelYearsArray.sort((a, b) => a - b);
   }
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.title = 'My travels';
     this.description = 'Website created for front-end technologies at university';
-    this.galleries = Galleries;
+    this.galleries = this.fetchGalleries();
     this.searchValue = '';
     this.createSortedYearsArray();
+  }
+
+  fetchGalleries() {
+    this.galleries = [];
+    this.http.get('http://project.usagi.pl/gallery', this.httpOptions).toPromise().then((response: IGallery[]) => {
+      console.log(response);
+      this.galleries = response;
+    });
+    return this.galleries;
   }
 
   setSearchValue($event) {
@@ -40,6 +60,19 @@ export class GalleriesComponent implements OnInit {
 
   setSearchYear($event) {
     this.yearSearch = $event;
+  }
+
+  exportGalleries() {
+    Galleries.forEach((gallery: IGallery) => {
+      delete (gallery.galleryId);
+
+      this.http.post('http://project.usagi.pl/gallery', gallery, this.httpOptions).toPromise().then((response: IGallery) => {
+        console.log('success', response);
+        this.galleries.push(response);
+      }, (errResponse) => {
+        console.log('error', errResponse);
+      });
+    });
   }
 
   ngOnInit(): void {
