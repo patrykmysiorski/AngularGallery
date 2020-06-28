55490cac5c438a6e11501048a5e635550cbd7b4b
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-28 12:36:47 +0200

    add mock photo fields

--- src/app/constants/galleries.constant.ts
+++ src/app/constants/galleries.constant.ts
@@ -1,27 +1,64 @@
 // TODO change values
 
-export const Galleries = [{
-  galleryId: '1',
-  title: 'Rzym',
-  dateCreated: '2015-12-15T00:00:00+00:00',
-  thumbUrl: './assets/img/galleries/box1/main.jpeg',
-  description: 'Kilka dni zwariowanego wypadu do Rzymu.',
-  tags: [],
-  photos: []
-}, {
-  galleryId: '2',
-  title: 'Maroko',
-  dateCreated: '2015-08-07T00:00:00+00:00',
-  thumbUrl: '/assets/img/galleries/box2/main.jpeg',
-  description: 'Tydzień zwiedzania południowego Maroka z ojcem.',
-  tags: [],
-  photos: []
-}, {
-  galleryId: '3',
-  title: 'Tajlandia',
-  dateCreated: '2014-11-10T00:00:00+00:00',
-  thumbUrl: '/assets/img/galleries/box3/main.jpeg',
-  description: 'Dwa tygodnie wycieczki po Tajlandii.',
-  tags: [],
-  photos: []
-}];
+export const Galleries = [
+  {
+    galleryId: '1',
+    title: 'Rzym',
+    dateCreated: '2015-12-15T00:00:00+00:00',
+    thumbUrl: './assets/img/galleries/box1/main.jpeg',
+    description: 'Kilka dni zwariowanego wypadu do Rzymu.',
+    tags: [
+      {
+        tag: 'Koloseum'
+      },
+      {
+        tag: 'Watykan'
+      }
+    ],
+    photos: [
+      {
+        photoId: '1',
+        thumbUrl: './assets/img/galleries/box1/main.jpeg',
+        imgUrl: './assets/img/galleries/box1/main.jpeg'
+      },
+      {
+        photoId: '2',
+        thumbUrl: './assets/img/galleries/box1/main.jpeg',
+        imgUrl: './assets/img/galleries/box1/main.jpeg'
+      },
+      {
+        photoId: '3',
+        thumbUrl: './assets/img/galleries/box1/main.jpeg',
+        imgUrl: './assets/img/galleries/box1/main.jpeg'
+      },
+      {
+        photoId: '4',
+        thumbUrl: './assets/img/galleries/box1/main.jpeg',
+        imgUrl: './assets/img/galleries/box1/main.jpeg'
+      },
+      {
+        photoId: '5',
+        thumbUrl: './assets/img/galleries/box1/main.jpeg',
+        imgUrl: './assets/img/galleries/box1/main.jpeg'
+      }
+    ]
+  },
+  {
+    galleryId: '2',
+    title: 'Maroko',
+    dateCreated: '2015-08-07T00:00:00+00:00',
+    thumbUrl: '/assets/img/galleries/box2/main.jpeg',
+    description: 'Tydzień zwiedzania południowego Maroka z ojcem.',
+    tags: [],
+    photos: []
+  },
+  {
+    galleryId: '3',
+    title: 'Tajlandia',
+    dateCreated: '2014-11-10T00:00:00+00:00',
+    thumbUrl: '/assets/img/galleries/box3/main.jpeg',
+    description: 'Dwa tygodnie wycieczki po Tajlandii.',
+    tags: [],
+    photos: []
+  }
+];
