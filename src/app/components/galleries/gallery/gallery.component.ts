adfb525fbc2969daadecae67acb6467495700000
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-28 12:08:52 +0200

    get gallery from path

--- src/app/components/galleries/gallery/gallery.component.ts
+++ src/app/components/galleries/gallery/gallery.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {IGallery} from '../../../IGallery';
+import {Galleries} from '../../../constants/galleries.constant';
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  private galleryId: string;
+  gallery: IGallery;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.galleryId = this.route.snapshot.paramMap.get('galleryId');
+    this.gallery = Galleries.find((item: IGallery) => item.galleryId === this.galleryId);
+    console.log(this.gallery);
   }
 
 }
