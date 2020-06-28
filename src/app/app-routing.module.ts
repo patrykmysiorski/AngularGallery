3c919dfb09d3504ad3269767c01e778302611d2e
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-28 11:59:46 +0200

    add route for gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {GalleriesComponent} from './components/galleries/galleries/galleries.component';
 import {DashboardComponent} from './components/dashboard/dashboard/dashboard.component';
+import {GalleryComponent} from './components/galleries/gallery/gallery.component';
 
 const routes: Routes = [
   {
     path: 'galleries',
     component: GalleriesComponent
+  },
+  {
+    path: 'galleries/:galleryId',
+    component: GalleryComponent
   },
   {
     path: 'dashboard',
