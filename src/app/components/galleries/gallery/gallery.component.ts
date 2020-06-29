fea55e34a427f938a5a15fe3d1a1e06f0bdf5d71
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-30 00:15:27 +0200

    handle add comment emitter

--- src/app/components/galleries/gallery/gallery.component.ts
+++ src/app/components/galleries/gallery/gallery.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {IGallery} from '../../../../intefaces/IGallery';
-import {Galleries} from '../../../constants/galleries.constant';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {IComment} from '../../../../intefaces/IComments';
 
 @Component({
   selector: 'app-gallery',
@@ -22,15 +22,25 @@ export class GalleryComponent implements OnInit {
 
   private galleryId: string;
   gallery: IGallery;
+  comments: IComment[];
 
+  updateComments(event) {
+    this.comments.push(event);
+  }
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.galleryId = this.route.snapshot.paramMap.get('galleryId');
     const url = 'http://project.usagi.pl/gallery/' + this.galleryId;
     this.http.get(url, this.httpOptions).toPromise().then((response: IGallery) => {
       this.gallery = response;
+    });
+    const urlForComments = 'http://project.usagi.pl/comment/byGallery/' + this.galleryId;
+    this.http.get(urlForComments, this.httpOptions).toPromise().then((response: IComment[]) => {
+      console.log('response', response);
+      this.comments = response;
     });
   }
 
