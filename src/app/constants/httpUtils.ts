37e93fe1120711852d2bfec82c88a0d194f6b888
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-30 00:31:24 +0200

    move reusable code to separate file

---
+++ src/app/constants/httpUtils.ts
@@ -0,0 +1,10 @@
+import {HttpHeaders} from '@angular/common/http';
+
+export const httpOptions = {
+  headers: new HttpHeaders(
+    {
+      'Content-Type': 'application/json',
+      Authorization: '104'
+    }
+  )
+};
