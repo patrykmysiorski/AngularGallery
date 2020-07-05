6761815abf9a5c41415624bdc65277fae58118bb
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-07-05 20:27:31 +0200

    add news interface

---
+++ src/intefaces/INews.ts
@@ -0,0 +1,10 @@
+import {IPhoto} from './IPhoto';
+
+export interface INews {
+  newsId: string;
+  title: string;
+  shortContent: string;
+  fullContent: string;
+  dateCreated: Date;
+  photo?: IPhoto;
+}
