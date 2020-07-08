b90eb987143e49961d5dfdc190f5a045311d166f
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-07-08 22:53:32 +0200

    add news form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
 import {FormsModule} from '@angular/forms';
-import { SearchGalleriesPipe } from './pipes/search-galleries.pipe';
-import { SearchByYearPipe } from './pipes/search-by-year.pipe';
-import { GalleriesComponent } from './components/galleries/galleries/galleries.component';
-import { GalleryItemComponent } from './components/galleries/gallery-item/gallery-item.component';
-import { GallerySearchComponent } from './components/galleries/gallery-search/gallery-search.component';
-import { GallerySearchByYearComponent } from './components/galleries/gallery-search-by-year/gallery-search-by-year.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
-import { GalleryComponent } from './components/galleries/gallery/gallery.component';
-import { CommentFormComponent } from './components/comments/comment-form/comment-form.component';
-import { CommentsContainerComponent } from './components/comments/comments-container/comments-container.component';
-import { GalleryTagFormComponent } from './components/galleries/gallery-tag-form/gallery-tag-form.component';
-import { GalleryPhotoFormComponent } from './components/galleries/gallery-photo-form/gallery-photo-form.component';
-import { GalleryFormComponent } from './components/galleries/gallery-form/gallery-form.component';
+import {SearchGalleriesPipe} from './pipes/search-galleries.pipe';
+import {SearchByYearPipe} from './pipes/search-by-year.pipe';
+import {GalleriesComponent} from './components/galleries/galleries/galleries.component';
+import {GalleryItemComponent} from './components/galleries/gallery-item/gallery-item.component';
+import {GallerySearchComponent} from './components/galleries/gallery-search/gallery-search.component';
+import {GallerySearchByYearComponent} from './components/galleries/gallery-search-by-year/gallery-search-by-year.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardComponent} from './components/dashboard/dashboard/dashboard.component';
+import {GalleryComponent} from './components/galleries/gallery/gallery.component';
+import {CommentFormComponent} from './components/comments/comment-form/comment-form.component';
+import {CommentsContainerComponent} from './components/comments/comments-container/comments-container.component';
+import {GalleryTagFormComponent} from './components/galleries/gallery-tag-form/gallery-tag-form.component';
+import {GalleryPhotoFormComponent} from './components/galleries/gallery-photo-form/gallery-photo-form.component';
+import {GalleryFormComponent} from './components/galleries/gallery-form/gallery-form.component';
+import {NewsFormComponent} from './components/dashboard/news-form/news-form.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { GalleryFormComponent } from './components/galleries/gallery-form/galler
     CommentsContainerComponent,
     GalleryTagFormComponent,
     GalleryPhotoFormComponent,
-    GalleryFormComponent
+    GalleryFormComponent,
+    NewsFormComponent
   ],
   imports: [
     BrowserModule,
@@ -46,4 +48,5 @@ import { GalleryFormComponent } from './components/galleries/gallery-form/galler
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
