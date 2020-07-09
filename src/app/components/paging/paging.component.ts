9039ba4c682e5b85c4becdaf45a9bad2e1a0e0b9
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-07-09 20:56:13 +0200

    move paging to separate component

--- src/app/components/paging/paging.component.ts
+++ src/app/components/paging/paging.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-paging',
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagingComponent implements OnInit {
 
-  constructor() { }
+  @Output()
+  moveForward = new EventEmitter();
+
+  @Output()
+  moveBack = new EventEmitter();
+
+  @Output()
+  setCurrentPage = new EventEmitter();
+
+  @Input() itemsList: any;
+  @Input() currentPage: number;
+  @Input() numberOfPages: [];
+
+  onMoveBack() {
+    this.moveBack.emit(this.currentPage - 1);
+  }
+
+  onSetCurrentPage(numberOfPage) {
+    this.setCurrentPage.emit(numberOfPage);
+  }
+
+  onMoveForward() {
+    this.moveForward.emit(this.currentPage + 1);
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
