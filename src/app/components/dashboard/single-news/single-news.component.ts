a9ca24b1f07390729bd4d858535305a399c0416b
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-07-09 18:22:28 +0200

    add and style cingle news component

--- src/app/components/dashboard/single-news/single-news.component.ts
+++ src/app/components/dashboard/single-news/single-news.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {INews} from '../../../../intefaces/INews';
 
 @Component({
   selector: 'app-single-news',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-news.component.scss']
 })
 export class SingleNewsComponent implements OnInit {
+
+  @Input() news: INews;
 
   constructor() { }
 
